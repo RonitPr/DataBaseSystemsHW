68b15d5bb363fa1071e69b5c5c2cb46f2fceb189
RonitPriav <42570726+RonitPr@users.noreply.github.com>
2021-12-17 00:17:21 +0200

    changed funcnames to snake format

--- db/src/CREATE_DB_SCRIPT.py
+++ db/src/CREATE_DB_SCRIPT.py
@@ -1,8 +1,8 @@
 import mysql.connector
-from environment import config, base_url
+from environment import config
 
 
-def createTable(cursor, tableName, optionDict):
+def create_table(cursor, tableName, optionDict):
     # execute on cursor the creation of a table with tableName with the columns described in colDict
     columns = ""
     for key in optionDict.keys():
@@ -11,12 +11,12 @@ def createTable(cursor, tableName, optionDict):
     cursor.execute(f"CREATE TABLE {tableName} ({columns}) ENGINE=InnoDB")
 
 
-def createDatabase():
+def create_database():
     # create connection and cursor to create the tables on.
     con = mysql.connector.connect(**config())
     with con:
         cursor = con.cursor()
-        createTable(cursor, 'movie', {
+        create_table(cursor, 'movie', {
             'movie_id': 'CHAR(10)',
             'title': 'VARCHAR(255) NOT NULL',
             'year': 'YEAR(4) NOT NULL',
@@ -30,13 +30,13 @@ def createDatabase():
 
         # add the fulltext reverse index later, after the db is stable.
 
-        createTable(cursor, 'genre', {
+        create_table(cursor, 'genre', {
             'genre_id': 'TINYINT(10)',
             'name': 'CHAR(20) NOT NULL',
             'PRIMARY KEY': '(genre_id)'
         })
 
-        createTable(cursor, 'movie_genre', {
+        create_table(cursor, 'movie_genre', {
             'movie_id': 'CHAR(10)',
             'genre_id': 'TINYINT(10)',
             'PRIMARY KEY': '(movie_id,genre_id)',
@@ -44,13 +44,13 @@ def createDatabase():
             'FOREIGN KEY (movie_id)': 'REFERENCES movie(movie_id)'
         })
 
-        createTable(cursor, 'director', {
+        create_table(cursor, 'director', {
             'director_id': 'SMALLINT',
             'name': 'VARCHAR(255) NOT NULL',
             'PRIMARY KEY': '(director_id)'
         })
 
-        createTable(cursor, 'movie_director', {
+        create_table(cursor, 'movie_director', {
             'movie_id': 'CHAR(10)',
             'director_id': 'SMALLINT',
             'PRIMARY KEY': '(imdb_id,director_id)',
@@ -58,13 +58,13 @@ def createDatabase():
             'FOREIGN KEY (movie_id)': 'REFERENCES movie(movie_id)'
         })
 
-        createTable(cursor, 'actor', {
+        create_table(cursor, 'actor', {
             'actor_id': 'SMALLINT',
             'name': 'VARCHAR(255) NOT NULL',
             'PRIMARY KEY': '(actor_id)'
         })
 
-        createTable(cursor, 'movie_actor', {
+        create_table(cursor, 'movie_actor', {
             'movie_id': 'CHAR(10)',
             'actor_id': 'SMALLINT',
             'PRIMARY KEY': '(imdb_id,actor_id)',
@@ -73,7 +73,7 @@ def createDatabase():
         })
 
 
-def addIndex():
+def add_index():
     # after the DB is stable and we added the data, create the indexes.
     con = mysql.connector.connect(**config())
     with con:
@@ -81,4 +81,4 @@ def addIndex():
 
 
 if __name__ == '__main__':
-    createDatabase()
+    create_database()
